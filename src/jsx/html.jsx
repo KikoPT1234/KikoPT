9cc554bc81deadc1dbb62f62b8c960b306d79436
KikoPT1234 <carricokiko2006@gmail.com>
2020-03-09 22:09:00 +0100

    Update

--- src/jsx/html.jsx
+++ src/jsx/html.jsx
@@ -1,10 +1,52 @@
+class Topnav extends React.Component {
+    render() {
+        return (
+            <div className="topnav">
+                <ul>
+                    <li>Minehut</li>
+                    <li>Other Stuff</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div className="header">
+                <div className="topnav">
+                    <Topnav />
+                </div>
+                <div className="banner">
+                    <div className="img"></div>
+                    <div className="title"></div>
+                </div>
+            </div>
+        )
+    }
+}
+
+class Container extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <h1>Hello, my name is Kiko, I'm a Node.JS and Web Developer.</h1>
+            </div>
+        )
+    }
+}
+
 class Page extends React.Component {
     render() {
         return (
-            <h1>Hello. I'm Kiko.</h1>
+            <div className=".root">
+                <Header />
+                <Container />
+            </div>
         )
     }
 }
 
 // =========================================
-ReactDOM.render(<Page />, document.querySelector(".root"))
+ReactDOM.render(<Page />, document.querySelector("body"))
