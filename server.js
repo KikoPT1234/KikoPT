f305f8a403ab5d20a0994140b20787b248741b0c
KikoPT1234 <carricokiko2006@gmail.com>
2020-03-09 21:47:40 +0100

    Hopefully last

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express")
 const app = express()
 app.listen(process.env.PORT || 80)
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
     res.sendFile(`${__dirname}/views/index.html`)
 })
---
+++ src/jsx/html.jsx
@@ -0,0 +1,10 @@
+class Page extends React.Component {
+    render() {
+        return (
+            <h1>Hello. I'm Kiko.</h1>
+        )
+    }
+}
+
+// =========================================
+ReactDOM.render(<Page />, document.querySelector(".root"))
