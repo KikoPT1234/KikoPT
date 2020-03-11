8d2bcbf8b8a1da23c9c768678d73e6d4a94c48be
KikoPT1234 <carricokiko2006@gmail.com>
2020-03-11 18:56:16 +0100

    Update

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require("express")
 const app = express()
 app.listen(process.env.PORT || 80)
 
-app.use(express.static("public"));
-app.use(function (request, response, next) {
+/*app.use(function (request, response, next) {
     if (!request.secure) {
         response.redirect("https://" + request.headers.host + request.url);
     }
     next()
-});
+});*/
+
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.sendFile(`${__dirname}/views/home.html`)
