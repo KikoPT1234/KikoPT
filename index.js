b18c8a5c6c9856365568e31dfcb5fcd90f0a7e1b
KikoPT1234 <carricokiko2006@gmail.com>
2020-03-18 23:46:48 +0100

    docs

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const app = require("./server.js")
+const fs = require("fs")
+
+const docs = require("./docs.json")
+const newDocs = {}
+for (let i in docs) {
+    if (!docs[i].endpoint.startsWith("https://api.minehut.com/admin")) newDocs[i] = docs[i]
+}
+console.log(newDocs)
