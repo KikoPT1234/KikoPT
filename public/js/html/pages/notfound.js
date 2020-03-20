75a92597b28d4f1ea13a0a3276fff5d51afc8bc9
KikoPT1234 <carricokiko2006@gmail.com>
2020-03-20 23:44:43 +0100

    404

---
+++ public/js/html/pages/notfound.js
@@ -0,0 +1,46 @@
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var NotFound = function (_React$Component) {
+    _inherits(NotFound, _React$Component);
+
+    function NotFound() {
+        _classCallCheck(this, NotFound);
+
+        return _possibleConstructorReturn(this, (NotFound.__proto__ || Object.getPrototypeOf(NotFound)).apply(this, arguments));
+    }
+
+    _createClass(NotFound, [{
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "div",
+                { className: "notfound" },
+                React.createElement(
+                    "h1",
+                    null,
+                    "404 Not Found"
+                ),
+                React.createElement(
+                    "h2",
+                    null,
+                    "It seems you got lost, oops!"
+                ),
+                React.createElement(
+                    "a",
+                    { href: "home" },
+                    "Home"
+                )
+            );
+        }
+    }]);
+
+    return NotFound;
+}(React.Component);
+
+ReactDOM.render(React.createElement(NotFound, null), document.querySelector("body"));
--- server.js
+++ server.js
@@ -28,6 +28,26 @@ app.get("/minehut/api/json", (req, res) => {
     res.status(200).json(require(`./docs.json`))
 })
 
+app.use("/", (req, res, next) => {
+    const error = new Error("Not found");
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    if (error.status === 404) {
+        res.sendFile(__dirname + "/views/notfound.html")
+    }
+    else {
+        res.json({
+            error: {
+                message: error.message
+            }
+        })
+    }
+})
+
 app.listen(process.env.PORT || 80)
 
 module.exports = app
